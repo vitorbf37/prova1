b2c12e3bee396e66ad2eb5cf7cb19543274fabfc
Vitor Braga <vitorbf37@gmail.com>
2020-05-15 20:47:19 -0300

    Prova completa

---
+++ src/pro1/exercicio.java
@@ -0,0 +1,15 @@
+package pro1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("SISTEMA DE PERFIL DE CLIENTE%n--------------------------------%nINFORME OS DADOS DO ÚLTIMO ANO%n%n");
+	}
+
+}
